94aec439123f94c5fff4f554ba5efcf50d7638b3
dollargut <dollargut123@gmail.com>
2025-03-20 01:18:54 +0900

    ViewModel 적용 전 screen

--- Tlog/app/src/main/java/com/tlog/ui/screen/TbtiResultScreen.kt
+++ Tlog/app/src/main/java/com/tlog/ui/screen/TbtiResultScreen.kt
@@ -14,11 +14,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
@@ -39,30 +43,63 @@ import com.tlog.R
 import com.tlog.ui.component.MainButton
 
 
-// 상단 네비바 추가 필요 피그마 -> 아이콘클릭 -> 우측하단 export -> svg로 다운로드 -> drawable에서 우클릭 후 vector 추가 해서 사용
 @Preview
 @Composable
-fun TbtiResultScreen(
-    leftLabels: List<String> = listOf("S", "L", "E", "A"), // 이건 지역변수로 사용해도 됨 (고정이라 의미없음)
-    rightLabels: List<String> = listOf("R", "O", "N", "I"),
-    leftProgress: List<Float> = listOf(0.5f, 0.7f, 0.6f, 0.4f), // 이것도 좌측 비율만 가져와서 연산해서 바꾸면 됨
-    rightProgress: List<Float> = listOf(0.6f, 0.3f, 0.8f, 0.9f)
+fun TbtiResultScreen() {
+    val scrollState = rememberScrollState()
 
-) {
-    val scrollState = rememberScrollState() // 이건 스크롤 가능하게 하는건가?
+    val leftLabels = listOf("S", "L", "E", "A")
+    val rightLabels = listOf("R", "O", "N", "I")
+    val leftProgress = listOf(0.5f, 0.7f, 0.6f, 0.4f)
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.White)
             .verticalScroll(scrollState)
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // 상단 네비게이션 바
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 45.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_left_arrow),
+                contentDescription = "뒤로가기",
+                modifier = Modifier
+                    .width(42.dp)
+                    .height(42.dp)
+            )
+
+            Row {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_download),
+                    contentDescription = "저장",
+                    modifier = Modifier
+                        .width(42.dp)
+                        .height(42.dp)
+                        .padding(end = 12.dp),
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_share),
+                    contentDescription = "공유",
+                    modifier = Modifier
+                        .width(42.dp)
+                        .height(42.dp)
+                )
+            }
+        }
+
         Spacer(modifier = Modifier.height(15.dp))
-        // 상단 대표 이미지
+
         Image(
-            painter = painterResource(id = R.drawable.test_image), // 직접 넣은 이미지 사용
-            contentDescription = "TBTI 대표 이미지", // 설명 TBTI 캐릭터 이미지로 수정하면 좋을 듯
+            painter = painterResource(id = R.drawable.test_image),
+            contentDescription = "TBTI 캐릭터 이미지",
             modifier = Modifier
                 .fillMaxWidth()
                 .height(200.dp),
@@ -71,80 +108,94 @@ fun TbtiResultScreen(
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // 결과 텍스트
-        Text(text = "RENA", fontSize = 32.sp, fontWeight = FontWeight.Bold)
-        Text(text = "안정적인 자연 탐험가", fontSize = 18.sp, color = Color.Gray)
+        Text(text = "RENA",
+            fontFamily = MainFont,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.ExtraBold)
+        Text(text = "안정적인 자연 탐험가",
+            fontFamily = MainFont,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium
+            )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        //각각 성향의 퍼센트를 나타내는 부분
-        for (i in 0 until 4) {
-            val maxProgress = maxOf(leftProgress[i], rightProgress[i]) // 더 큰 값을 저장
-            val animatedProgress by animateFloatAsState( // 이 스크린이 열릴 때도 애니메이션이 적용되는지? -> 있으면 유지, 없으면 제거
-                targetValue = maxProgress,
-                animationSpec = tween(800),
-                label = "animated-bar-$i"
-            )
-
+        // 프로그레스 바
+        for (i in leftLabels.indices) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 6.dp), // 여백 더 주자 너무 좁음 + 좌우 여백도 들어가면 좋을 듯 ? 너무 빽빽하다 피그마 참고 !
-                verticalAlignment = Alignment.CenterVertically
+                    .padding(vertical = 23.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text(leftLabels[i]) // 왼쪽 라벨
+                Text(
+                    text = leftLabels[i],
+                    modifier = Modifier.padding(end = 10.dp),
+                    fontFamily = MainFont,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 18.sp
+                )
 
                 LinearProgressIndicator(
-                    progress = { animatedProgress },
+                    progress = { leftProgress[i] },
                     modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 8.dp)
-                        .height(8.dp)
+                        .width(236.dp)
+                        .height(12.dp)
                         .clip(RoundedCornerShape(4.dp)),
                     color = Color.Blue,
                     trackColor = Color(0xFFE0E0E0)
                 )
 
-                Text(rightLabels[i]) // 오른쪽 라벨
+                Text(
+                    text = rightLabels[i],
+                    modifier = Modifier.padding(start = 10.dp),
+                    fontFamily = MainFont,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 18.sp
+                )
             }
         }
 
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // 설명 텍스트
         Text(
-            text = "RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서 RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서" +
-                    "RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서" +
-                    "RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서" +
-                    "RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서" +
-                    "RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서" +
-                    "RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서RENA는 00을 중요하게 생각하고, 여행할 땐 자연을 좋아해요. 그래서",
+            text = "RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며" +
+                    "RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며" +
+                    "RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며" +
+                    "RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며" +
+                    "RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며" +
+                    "RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며" +
+                    "RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며RENA는 자연을 좋아하고 안정적인 계획을 선호하는 유형입니다. 여행에서도 효율성과 여유를 동시에 추구하며",
+            fontFamily = MainFont,
             fontSize = 14.sp,
-            color = Color.DarkGray,
+            fontWeight = FontWeight.Normal,
             lineHeight = 20.sp
         )
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // 잘 맞는 / 안 맞는 TBTI + 이미지
+        // 잘 맞는 / 안 맞는 TBTI 카드
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 8.dp),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            // 잘 맞는 TBTI 카드
             Column(
                 modifier = Modifier
                     .weight(1f)
                     .padding(8.dp)
                     .clip(RoundedCornerShape(16.dp))
-                    .background(Color(0xFFF5F5F5))
+                    .background(Color(0xFFF1F4FD))
                     .padding(12.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("잘 맞는 TBTI", fontWeight = FontWeight.Medium)
+                Text("잘 맞는 TBTI",
+                    fontFamily = MainFont,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal)
                 Spacer(modifier = Modifier.height(8.dp))
                 Image(
                     painter = painterResource(id = R.drawable.test_image),
@@ -155,20 +206,25 @@ fun TbtiResultScreen(
                     contentScale = ContentScale.Crop
                 )
                 Spacer(modifier = Modifier.height(8.dp))
-                Text("RENA")
+                Text("RENA",
+                    fontFamily = MainFont,
+                    fontSize = 14.sp
+                )
             }
 
-            // 안 맞는 TBTI 카드
-            Column( // 영역을 잘 정했으나 백그라운드 컬러를 피그마랑 맞출 것
+            Column(
                 modifier = Modifier
                     .weight(1f)
                     .padding(8.dp)
                     .clip(RoundedCornerShape(16.dp))
-                    .background(Color(0xFFF5F5F5))
+                    .background(Color(0xFFF1F4FD))
                     .padding(12.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("안 맞는 TBTI", fontWeight = FontWeight.Medium)
+                Text("안 맞는 TBTI",
+                    fontFamily = MainFont,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal)
                 Spacer(modifier = Modifier.height(8.dp))
                 Image(
                     painter = painterResource(id = R.drawable.test_image),
@@ -179,18 +235,23 @@ fun TbtiResultScreen(
                     contentScale = ContentScale.Crop
                 )
                 Spacer(modifier = Modifier.height(8.dp))
-                Text("SALI")
+                Text("SALI",
+                    fontFamily = MainFont,
+                    fontSize = 14.sp
+                    )
             }
         }
 
-
         Spacer(modifier = Modifier.height(32.dp))
 
-        // 시작 버튼
         MainButton(
             text = "Tlog 시작하기",
-            onClick = { /* 메인화면으로 넘어가도록 */ } // 높이 65 / 좌우 여백도 15씩 modifier로 주면 됨
+            onClick = { /* 메인 화면 이동 처리 */ },
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, bottom = 50.dp)
+                .height(65.dp)
         )
-        Spacer(modifier = Modifier.height(5.dp))
+
+        Spacer(modifier = Modifier.height(32.dp))
     }
 }
