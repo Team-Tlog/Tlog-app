8ee421e2e439e5b485b77610d04de86f4e35e75d
DollarGut <165395314+DollarGut@users.noreply.github.com>
2025-03-23 17:57:15 +0900

    feat: 사용자가 tbti 테스트를 완료 후 마음에 드는 여행지 선택 스크린 구현
    * feat: 사용자가 tbti 테스트를 완료 후  마음에 드는 여행지 선택 스크린 구현
    
    * feat: ui shadow 적용, 실제 사진 적용하여 화면 테스트, 여행지 선택하면  테두리 색깔 변환
    
    ---------
    
    Co-authored-by: dollargut <dollargut123@gmail.com>
    Co-authored-by: 정찬 <125573608+cksckckcks@users.noreply.github.com>

--- Tlog/app/src/main/java/com/tlog/MainActivity.kt
+++ Tlog/app/src/main/java/com/tlog/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tlog.ui.screen.ChooseMyTypeDestinationScreen
 import com.tlog.ui.screen.AddTravelDestinationScreen
 import com.tlog.ui.screen.LoginScreen
 import com.tlog.ui.screen.ReviewWritingScreen
@@ -35,7 +36,9 @@ class MainActivity : ComponentActivity() {
             //TbtiResultScreen()
             //AddTravelDestinationScreen()
             //SelectReviewWriteScreen()
-            ReviewWritingScreen()
+            //ReviewWritingScreen()
+          ChooseMyTypeDestinationScreen()
+
         }
     }
 }
