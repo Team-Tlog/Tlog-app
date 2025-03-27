01c29a26907cb82d32bfb067594b12dcc0a606a7
csjws <163653896+csjws@users.noreply.github.com>
2025-03-28 02:02:55 +0900

    Feat: 팀 이름 정하기 화면 구현(팀 생성) (#43)
    * feat: 팀 이름 정하기 화면 구현, 팀 이름 텍스트필드 component 구현, 팀이름뷰모델 구현
    
    * feat: 팀 생성하기 버튼 구현, sp 값 변경
    
    * feat: 키보드가 올라왔을때 버튼도 같이 올라오게 구현
    
    * 테스트를 위해 메인에 코드 한줄 추가
    
    ---------
    
    Co-authored-by: dollargut <dollargut123@gmail.com>
    Co-authored-by: DollarGut <165395314+DollarGut@users.noreply.github.com>

---
+++ Tlog/app/src/main/java/com/tlog/ui/screen/TeamNameCreateScreen.kt
@@ -0,0 +1,84 @@
+package com.tlog.ui.screen
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tlog.ui.component.HashtagInputGroup
+import com.tlog.ui.component.MainButton
+import com.tlog.ui.component.MainInputField
+import com.tlog.ui.component.PhotoUploadBox
+import com.tlog.ui.component.TeamNameInputField
+import com.tlog.ui.component.TopBar
+import com.tlog.ui.component.TwoColumnRadioGroup
+import com.tlog.ui.theme.MainColor
+import com.tlog.viewmodel.AddTravelViewModel
+import com.tlog.viewmodel.TeamNameViewModel
+
+
+@Preview
+@Composable
+fun TeamNameCreateScreen(viewModel: TeamNameViewModel = viewModel()) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .imePadding()           // 키보드가 딸려 올라오도록
+            .windowInsetsPadding(WindowInsets.systemBars)
+
+    ) {
+        TopBar(
+            text = "팀 생성"
+        )
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        TeamNameInputField(
+            text = "팀 이름을 정해주세요!",
+            value = viewModel.TeamName.value,
+            onValueChange = {
+                viewModel.updateTeamName(it)
+                Log.d("TeamNameValue", viewModel.TeamName.value)
+            },
+            placeholderText = "입력해주세요",
+
+        )
+
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        // 입력값이 있을 때만 버튼 표시
+        if (viewModel.TeamName.value.isNotEmpty()) {
+            MainButton(
+                text = "팀 생성하기",
+                onClick = { Log.d("Team Create", "my click!!") },
+                modifier = Modifier
+                    .height(70.dp)
+                    .padding(start = 10.dp, end = 10.dp, bottom = 15.dp)
+            )
+        }
+    }
+}
