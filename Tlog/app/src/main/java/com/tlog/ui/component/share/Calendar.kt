e33f7e401381f67dcfe655cd2fa69a1a7c0644be
정찬 <chan679999@naver.com>
2025-03-29 17:15:10 +0900

    fix: 1일이 일요일 경우 1주만큼 Row가 공백으로 들어가는 오류 수정

--- Tlog/app/src/main/java/com/tlog/ui/component/share/Calendar.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/share/Calendar.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tlog.ui.theme.MainFont
@@ -57,7 +56,8 @@ fun Calendar(
         // 날짜 설정 부
         val days = mutableListOf<String>()
 
-        repeat(dayOfWeek) { days.add("") } // 첫 주 시작점 밸런스 맞추기 repeat(n) -> 코드를 n번 실행
+        if (dayOfWeek != 7)
+            repeat(dayOfWeek) { days.add("") } // 첫 주 시작점 밸런스 맞추기 repeat(n) -> 코드를 n번 실행
 
         for (day in 1..monthLength)
             days.add(day.toString())
