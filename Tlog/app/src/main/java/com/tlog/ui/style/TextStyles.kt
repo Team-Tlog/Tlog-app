fcc942deca918ecee827e3c0401ab0cfcd335227
정찬 <chan679999@naver.com>
2025-03-31 23:53:58 +0900

    feat: 폰트 스타일 추가

---
+++ Tlog/app/src/main/java/com/tlog/ui/style/TextStyles.kt
@@ -0,0 +1,37 @@
+package com.tlog.ui.style
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.tlog.ui.theme.MainFont
+
+
+val Title = TextStyle(
+    fontFamily = MainFont,
+    fontSize = 30.sp,
+    fontWeight = FontWeight.ExtraBold
+)
+
+val Body1Regular = TextStyle(
+    fontFamily = MainFont,
+    fontSize = 14.sp,
+    fontWeight = FontWeight.Normal
+)
+
+val Body2Regular = TextStyle(
+    fontFamily = MainFont,
+    fontSize = 12.sp,
+    fontWeight = FontWeight.Light
+)
+
+val BodyTitle = TextStyle(
+    fontFamily = MainFont,
+    fontSize = 24.sp,
+    fontWeight = FontWeight.ExtraBold
+)
+
+val Body1Bold = TextStyle(
+    fontFamily = MainFont,
+    fontSize = 15.sp,
+    fontWeight = FontWeight.Bold
+)
