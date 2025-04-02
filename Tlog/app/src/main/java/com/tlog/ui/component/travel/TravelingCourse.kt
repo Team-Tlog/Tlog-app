8d580a5124718082a99ce068b43f5fab5ae378e0
정찬 <chan679999@naver.com>
2025-04-02 21:33:20 +0900

    fix: 패키지 이름 변경으로 인한 import 수정

--- Tlog/app/src/main/java/com/tlog/ui/component/travel/TravelingCourse.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/travel/TravelingCourse.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
 import androidx.compose.material3.Text
-import com.tlog.data.model.TravelUiData
+import com.tlog.data.model.travel.TravelUiData
 import com.tlog.ui.component.travel.DayToggleBar
 import com.tlog.ui.style.Body1Bold
 
