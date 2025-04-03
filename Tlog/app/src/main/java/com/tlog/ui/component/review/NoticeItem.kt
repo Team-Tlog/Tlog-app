e9f127ccf5019cc129ca6c3a85c9367cb0f9dcb8
dollargut <dollargut123@gmail.com>
2025-04-03 16:31:31 +0900

    feat: 경고 팝업 안에 텍스트 정렬

--- Tlog/app/src/main/java/com/tlog/ui/component/review/NoticeItem.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/review/NoticeItem.kt
@@ -3,9 +3,11 @@ package com.tlog.ui.component.review
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tlog.ui.theme.MainFont
@@ -15,31 +17,34 @@ fun NoticeItem(
     title: String,
     body: String
 ) {
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "✅",
+                fontSize = 14.sp,
+                modifier = Modifier.padding(end = 6.dp)
+            )
+            Text(
+                text = title,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = MainFont,
+                textAlign = TextAlign.Center,
+                color = Color.Black
+            )
+        }
+
+        Spacer(modifier = Modifier.height(4.dp))
+
         Text(
-            text = "✅",
-            fontSize = 14.sp,
-            modifier = Modifier.padding(end = 6.dp)
-        )
-        Text(
-            text = title,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.SemiBold,
+            text = body,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Light,
             fontFamily = MainFont,
-            color = Color.Black
+            color = Color.Black,
         )
+
+        Spacer(modifier = Modifier.height(14.dp))
     }
-
-    Spacer(modifier = Modifier.height(4.dp))
-
-    Text(
-        text = body,
-        fontSize = 12.sp,
-        fontWeight = FontWeight.Light,
-        fontFamily = MainFont,
-        color = Color.Black,
-        modifier = Modifier.padding(start = 22.dp) // 체크모양 맞춰 들여쓰기
-    )
-
-    Spacer(modifier = Modifier.height(14.dp))
 }
