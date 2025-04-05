e5e17c317b628c30c4678f920a219ed4d8976f0e
정찬 <chan679999@naver.com>
2025-04-05 21:25:31 +0900

    feat: TBTI 테스트 스크린 컴포넌트화

---
+++ Tlog/app/src/main/java/com/tlog/ui/component/tbti/TbtiTestAnswerBox.kt
@@ -0,0 +1,58 @@
+package com.tlog.ui.component.tbti
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tlog.ui.theme.MainColor
+import com.tlog.ui.theme.MainFont
+
+
+@Composable
+fun TbtiTestAnswerBox(
+    answer: String
+) {
+    var color by remember { mutableStateOf(Color.White) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(1.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .clickable {
+                Log.d("choose2", "my click!!")
+                color = if (color == Color.White) MainColor else Color.White
+            }
+            .height(58.dp)
+    ) {
+        Text(
+            text = answer,
+            fontFamily = MainFont,
+            fontWeight = FontWeight.Medium,
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center,
+            color = if (color == Color.White) Color.Black else Color.White,
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+    }
+}
