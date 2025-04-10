ea82df05401a86f6fc20ae0705c921520ce924ac
cksckckcks <chan679999@naver.com>
2025-04-10 17:55:40 +0900

    feat: 다른코스 추가

--- Tlog/app/src/main/java/com/tlog/ui/component/sns/RecentTravelCourseGroup.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/sns/RecentTravelCourseGroup.kt
@@ -1,16 +1,21 @@
 package com.tlog.ui.component.sns
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,9 +23,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tlog.R
 import com.tlog.ui.style.Body2Regular
+import com.tlog.ui.theme.MainColor
+import com.tlog.ui.theme.MainFont
 import com.tlog.viewmodel.sns.SnsPostViewModel
 
 
@@ -29,7 +39,8 @@ fun RecentTravelCourseGroup(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         Text(
             text = "최근 다녀온 코스",
@@ -38,9 +49,9 @@ fun RecentTravelCourseGroup(
                 .padding(start = 4.dp)
         )
 
-        Spacer(modifier = Modifier.height(4.dp))
-
         RecentTravelCourse()
+
+        OtherCourseSection()
     }
 }
 
@@ -76,5 +87,34 @@ fun RecentTravelCourse(
                 )
             }
         }
+    }
+}
+
+@Composable
+fun OtherCourseSection() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                Log.d("다른 코스", "my click!!")
+            }
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            text = "다른 코스",
+            fontFamily = MainFont,
+            fontSize = 13.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = MainColor
+        )
+
+        Icon(
+            painter = painterResource(R.drawable.ic_right_arrow),
+            contentDescription = "다른 코스",
+            tint = Color.Unspecified,
+            modifier = Modifier
+                .size(20.dp)
+        )
     }
 }
