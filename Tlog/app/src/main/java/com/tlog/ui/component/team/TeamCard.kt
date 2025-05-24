0848f370b74e8d881ad7007e2a5951374ae30297
cksckckcks <chan679999@naver.com>
2025-05-24 14:24:54 +0900

    refact: 필요없는 import 제거

--- Tlog/app/src/main/java/com/tlog/ui/component/team/TeamCard.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/team/TeamCard.kt
@@ -1,18 +1,14 @@
 package com.tlog.ui.component.team
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
