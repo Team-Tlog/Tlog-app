fabbb446c3e404df5c285a39f6b6c0f7f6c1da87
정찬 <125573608+cksckckcks@users.noreply.github.com>
2025-05-25 22:00:24 +0900

    feat: 메인 스크린 작성 (#116)
    * feat: 메인 스크린 상단바 개발
    
    * feat: 메인 스크린 추천 박스 개발
    
    * feat: 메인 스크린 아이콘 배너 개발
    
    * feat: 메인 스크린 지역별 여행지 추천 개발
    
    * feat: 메인 스크린 지역별 코스 추천 개발
    
    * feat: 메인 스크린 지역별 추천 여행지 개발
    
    * feat: 메인 스크린 지역별 인기 게시글 작성
    
    * feat: 메인 스크린 지역별 이슈 작성
    
    * feat: 메인 스크린 작성 + Scaffold로 구조 변경 (탑바, 바텀바 고정에 용이)

--- Tlog/app/src/main/java/com/tlog/ui/navigation/NavHost.kt
+++ Tlog/app/src/main/java/com/tlog/ui/navigation/NavHost.kt
@@ -10,6 +10,7 @@ import com.tlog.ui.screen.review.AddTravelDestinationScreen
 import com.tlog.ui.screen.review.ReviewWritingScreen
 import com.tlog.ui.screen.review.SelectReviewWriteScreen
 import com.tlog.ui.screen.share.CartScreen
+import com.tlog.ui.screen.share.MainScreen
 import com.tlog.ui.screen.share.NotificationScreen
 import com.tlog.ui.screen.sns.SnsPostWriteDetailScreen
 import com.tlog.ui.screen.team.MyTeamListScreen
@@ -24,10 +25,10 @@ fun NavHost(
     navController: NavHostController,
     mainViewModel: MainViewModel
 ) {
-    NavHost(navController = navController, startDestination = "searchReview") {
+    NavHost(navController = navController, startDestination = "main") {
         composable("main") {
             // 메인화면으로 수정해야함
-            NotificationScreen(navController, previousSelectedIndex = 0)
+            MainScreen(navController = navController)
         }
         composable("course") {
             //MyTravelingCourse는 어떻게?
