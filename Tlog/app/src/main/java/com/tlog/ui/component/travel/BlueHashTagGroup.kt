6337c022d1896480a4a77d002d1bfe512dd8c638
정찬 <125573608+cksckckcks@users.noreply.github.com>
2025-06-23 21:33:25 +0900

    feat: hashTag 그룹 업데이트 (#183)

--- Tlog/app/src/main/java/com/tlog/ui/component/travel/BlueHashTagGroup.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/travel/BlueHashTagGroup.kt
@@ -1,25 +1,11 @@
 package com.tlog.ui.component.travel
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.tlog.ui.theme.BackgroundBlue
-import com.tlog.ui.theme.Essential
-import com.tlog.ui.theme.MainFont
+import com.tlog.ui.component.share.BlueHashTag
 
 @Composable
 fun BlueHashTagGroup(
@@ -36,29 +22,7 @@ fun BlueHashTagGroup(
 
             val tag = hashTags[i]
 
-            Box(
-                modifier = Modifier
-                    //.fillMaxWidth()
-                    .clip(RoundedCornerShape(4))
-                    .background(BackgroundBlue)
-            ) {
-                Box(
-                    modifier = Modifier
-                        //.fillMaxSize()
-                        .height(22.dp)
-                        .padding(horizontal = 6.dp, vertical = 2.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = tag,
-                        fontSize = 10.sp,
-                        fontFamily = MainFont,
-                        fontWeight = FontWeight.Medium,
-                        textAlign = TextAlign.Center,
-                        color = Essential
-                    )
-                }
-            }
+            BlueHashTag(tag = tag)
         }
     }
 }
