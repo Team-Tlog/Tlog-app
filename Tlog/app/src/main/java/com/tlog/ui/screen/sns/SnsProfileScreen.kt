bed9eca6bd72c2b6690f105e8a90e4c947726e39
cksckckcks <chan679999@naver.com>
2025-06-25 16:35:36 +0900

    refact: 필요없는 import 제거

--- Tlog/app/src/main/java/com/tlog/ui/screen/sns/SnsProfileScreen.kt
+++ Tlog/app/src/main/java/com/tlog/ui/screen/sns/SnsProfileScreen.kt
@@ -1,6 +1,5 @@
 package com.tlog.ui.screen.sns
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
