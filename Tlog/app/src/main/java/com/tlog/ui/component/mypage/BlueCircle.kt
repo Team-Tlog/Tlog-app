d8b44fe6f520c9b27cdc3234a3167a421491abe7
cksckckcks <chan679999@naver.com>
2025-06-26 16:40:28 +0900

    refact: 필요없는 import 제거

--- Tlog/app/src/main/java/com/tlog/ui/component/mypage/BlueCircle.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/mypage/BlueCircle.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
