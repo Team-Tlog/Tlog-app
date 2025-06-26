10dfa4cba7994a8efeea9ec7eebc665a0973d60b
cksckckcks <chan679999@naver.com>
2025-06-26 15:46:17 +0900

    refact: 필요 없는 import 제거

--- Tlog/app/src/main/java/com/tlog/ui/component/share/TitleInputField.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/share/TitleInputField.kt
@@ -1,27 +1,14 @@
 package com.tlog.ui.component.share
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tlog.ui.style.BodyTitle
-import com.tlog.ui.theme.MainColor
-import com.tlog.ui.theme.MainFont
 
 
 @Composable
