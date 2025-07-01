806563b842836abf2201aa7cfaac7d95441e9066
정찬 <125573608+cksckckcks@users.noreply.github.com>
2025-07-01 20:43:18 +0900

    feat: SNS ID 생성 스크린 UI 업데이트 / 중복 체크 로직 수정 (#199)
    * feat: SNS ID 생성 스크린 수정
    
    * fix: SNS ID 생성 viewModel 버그 수정 (409 뜨면 catch부로 넘어가서 UiEvent를 제대로 설정하지 못하는 버그 수정)

--- Tlog/app/src/main/java/com/tlog/ui/component/share/TitleInputField.kt
+++ Tlog/app/src/main/java/com/tlog/ui/component/share/TitleInputField.kt
@@ -1,27 +1,16 @@
 package com.tlog.ui.component.share
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tlog.ui.style.BodyTitle
-import com.tlog.ui.theme.MainColor
-import com.tlog.ui.theme.MainFont
 
 
 @Composable
@@ -32,23 +21,28 @@ fun TitleInputField (
     onValueChange: (String) -> Unit,
     placeholderText: String,
     singleLine: Boolean = true,
-    trailingIcon: @Composable (() -> Unit)? = null
+    spacerHeight: Dp = 25.dp,
 ) {
-    Text(
-        modifier = Modifier
-            .fillMaxWidth(),
-        text = text,
-        style = BodyTitle,
-        textAlign = TextAlign.Center
-    )
 
-    Spacer(modifier = Modifier.height(25.dp))
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = text,
+            style = BodyTitle,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(spacerHeight))
 
 
-    BottomLineInputField(
-        value = value,
-        onValueChange = onValueChange,
-        placeholder = placeholderText,
-        singleLine = singleLine,
-    )
+        BottomLineInputField(
+            value = value,
+            onValueChange = onValueChange,
+            placeholder = placeholderText,
+            singleLine = singleLine,
+        )
+    }
 }
