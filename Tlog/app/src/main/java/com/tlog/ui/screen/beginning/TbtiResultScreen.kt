197e714221d914068037432792dc77b6bcaff3b0
cksckckcks <chan679999@naver.com>
2025-09-03 14:13:57 +0900

    refact: 필요 없는 Import 제거

--- Tlog/app/src/main/java/com/tlog/ui/screen/beginning/TbtiResultScreen.kt
+++ Tlog/app/src/main/java/com/tlog/ui/screen/beginning/TbtiResultScreen.kt
@@ -1,6 +1,5 @@
 package com.tlog.ui.screen.beginning
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
