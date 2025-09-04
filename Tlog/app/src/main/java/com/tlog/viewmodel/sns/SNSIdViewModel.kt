ddb0254d329e3e152181189783484baf7e2585fc
cksckckcks <chan679999@naver.com>
2025-09-04 14:26:49 +0900

    feat: SNS ID 뷰모델 ErrorType 적용, UiEvent 업데이트, 리팩토링

--- Tlog/app/src/main/java/com/tlog/viewmodel/sns/SNSIdViewModel.kt
+++ Tlog/app/src/main/java/com/tlog/viewmodel/sns/SNSIdViewModel.kt
@@ -1,11 +1,11 @@
 package com.tlog.viewmodel.sns
 
-import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tlog.data.local.UserPreferences
+import com.tlog.data.model.share.toErrorMessage
 import com.tlog.data.repository.SnsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -33,39 +33,21 @@ class SNSIdViewModel @Inject constructor(
     val snsId: State<String> = _snsId
 
     // 중복 여부 (true: 중복됨, false: 사용 가능, null: 아직 입력 안함)
-    private val _isDuplicated = mutableStateOf<Boolean>(false)
+    private val _isDuplicated = mutableStateOf(false)
     val isDuplicated: State<Boolean?> = _isDuplicated
 
 
     fun updateSnsId(id: String) {
         viewModelScope.launch {
             try {
-                val result = repository.updateSnsId(id)
+                repository.updateSnsId(id)
 
-                when (result.status) {
-                    200 -> {
-                        _eventFlow.emit(UiEvent.ApiSuccess)
-                        userPreferences.setSnsId(id)
-                    }
-                    404 -> _eventFlow.emit(UiEvent.ApiError(result.message ?: "사용자 정보 없음"))
-                    409 -> {
-                        _eventFlow.emit(UiEvent.ApiError(result.message ?: "중복된 ID"))
-                        _isDuplicated.value = true
-                    }
-                    500 -> _eventFlow.emit(UiEvent.ApiError(result.message ?: "서버 오류"))
-                    else -> _eventFlow.emit(UiEvent.ApiError(result.message ?: "알 수 없는 오류"))
-                }
-            }
-            catch (e: HttpException) {
-                when (e.code()) {
-                    404 -> _eventFlow.emit(UiEvent.ApiError("사용자 정보 없음"))
-                    409 -> {
-                        _isDuplicated.value = true
-                    }
-                    500 -> _eventFlow.emit(UiEvent.ApiError("서버 오류"))
-                    else -> _eventFlow.emit(UiEvent.ApiError("알 수 없는 오류"))
-                }
-                Log.d("SNSIdViewModel", e.message ?: "알 수 없는 오류")
+                userPreferences.setSnsId(id)
+                _eventFlow.emit(UiEvent.ApiSuccess)
+            } catch (e: HttpException) {
+                _eventFlow.emit(UiEvent.ApiError(e.toErrorMessage()))
+            } catch (e: Exception) {
+                _eventFlow.emit(UiEvent.ApiError(e.toErrorMessage()))
             }
         }
     }
