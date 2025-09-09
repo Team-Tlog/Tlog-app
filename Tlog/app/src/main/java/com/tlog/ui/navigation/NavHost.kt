91a44dab47204e63c4285442d10fc13fda72f995
cksckckcks <chan679999@naver.com>
2025-09-09 15:45:17 +0900

    feat: TypeSafetyNav 적용

--- Tlog/app/src/main/java/com/tlog/ui/navigation/NavHost.kt
+++ Tlog/app/src/main/java/com/tlog/ui/navigation/NavHost.kt
@@ -11,6 +11,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import androidx.navigation.navArgument
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.tlog.api.retrofit.TokenProvider
@@ -65,6 +67,123 @@ fun NavHost(
 
 
     NavHost(navController = navController, startDestination = startScreen) {
+        // TypeSafety
+        composable<Screen.Main> {
+            MainScreen(navController = navController)
+        }
+        composable<Screen.Login> {
+            LoginScreen(
+                viewModel = loginViewModel,
+                onGoogleLoginClick = {
+                    val intent = googleSignInClient.signInIntent
+                    launcher.launch(intent)
+                },
+                navController = navController
+            )
+        }
+        composable<Screen.Map> { MapScreen() }
+
+        // SNS
+        composable<Screen.SnsMain> { SnsScreen(navController = navController) }
+        composable<Screen.SnsId> { SnsIdCreateScreen(navController = navController) }
+        composable<Screen.Sns> {
+            val snsId = viewModel.tokenProvider.getSnsId()
+            Log.d("sns", "snsId: ${snsId}")
+            if (snsId.isNullOrEmpty())
+                SnsIdCreateScreen(navController = navController)
+            else
+                SnsScreen(navController = navController)
+        }
+        composable<Screen.SnsPostDetail> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.SnsPostDetail>()
+            SnsDetailScreen(postId = args.postId, navController = navController)
+        }
+        composable<Screen.SnsSearch> { SnsSearchScreen(navController = navController) }
+        composable<Screen.SnsMyPage> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.SnsMyPage>()
+            SnsProfileScreen(navController = navController, userId = args.userId)
+        }
+        composable<Screen.SnsPostWrite> { SnsPostWriteDetailScreen() }
+
+        // Review
+        composable<Screen.ReviewWrite> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.ReviewWrite>()
+            ReviewWriteScreen(navController = navController, travelId = args.travelId, travelName = args.travelName)
+        }
+        composable<Screen.ReviewList> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.ReviewList>()
+            ReviewListScreen(navController = navController, travelId = args.travelId, travelName = args.travelName)
+        }
+
+        // Travel
+        composable<Screen.ScrapAndCart> { ScrapAndCartScreen(navController = navController) }
+        composable<Screen.AddTravel> { AddTravelScreen(navController = navController) }
+        composable<Screen.TravelList> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.TravelList>()
+            TravelListScreen(title = args.title, city = args.city, navController = navController)
+        }
+        composable<Screen.TravelInfo> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.TravelInfo>()
+            TravelDetailScreen(travelId = args.travelId, navController = navController)
+        }
+
+        // Team
+        composable<Screen.TeamList> { MyTeamListScreen(navController = navController) }
+        composable<Screen.CreateTeam> { TeamCreateScreen(navController = navController) }
+        composable<Screen.TeamInfoInput> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.TeamInfoInput>()
+            TeamInfoInputScreen(teamName = args.teamName, navController = navController)
+        }
+        composable<Screen.TeamDetail> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.TeamDetail>()
+            TeamDetailScreen(teamId = args.teamId)
+        }
+        composable<Screen.JoinTeam> { TeamJoinByCode(navController = navController) }
+
+        // Search
+        composable<Screen.Search> { TravelSearchScreen(navController = navController) }
+        composable<Screen.SearchReview> { ReviewSearchScreen(navController = navController) }
+
+        // TBTI
+        composable<Screen.TbtiTest> { TbtiTestScreen(navController) }
+        composable<Screen.TbtiCodeInput> {
+            val viewModel: TbtiCodeInputViewModel = hiltViewModel()
+            TbtiCodeInputScreen(viewModel = viewModel)
+        }
+        composable<Screen.TbtiIntro> { TbtiIntroScreen(navController = navController) }
+        composable<Screen.TbtiResult> { backStackEntry ->
+            val args = backStackEntry.toRoute<Screen.TbtiResult>()
+            val resultList = listOf(args.sValue, args.eValue, args.lValue, args.aValue)
+            var resultCode = ""
+            resultList.forEach { result ->
+                resultCode += if (result.length == 1) "0${result}" else result
+            }
+            val traitScoresMap = mapOf(
+                "R" to args.sValue.toInt(),
+                "E" to args.eValue.toInt(),
+                "N" to args.lValue.toInt(),
+                "A" to args.aValue.toInt()
+            )
+            TbtiResultScreen(
+                tbtiResult = args.tbtiResultCode,
+                tbtiResultCode = resultCode,
+                traitScores = traitScoresMap,
+                navController = navController
+            )
+        }
+
+        // MyPage
+        composable<Screen.MyPage> { MyPageScreen(navController = navController) }
+        composable<Screen.Report> { ReportToDeveloperScreen() }
+        composable<Screen.Course> { MyTravelingCourseScreen(navController) }
+        composable<Screen.Notification> {
+            NotificationScreen(navController = navController, previousSelectedIndex = 0)
+        }
+
+
+
+
+
         // Main
         composable("main") {
             MainScreen(navController = navController)
