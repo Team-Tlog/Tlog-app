ba72810da2a506a455505e729714a687449c187f
cksckckcks <chan679999@naver.com>
2025-11-08 00:47:47 +0900

    fix: index out of bounds 오류 해결 (SnsViewModel)

--- Tlog/app/src/main/java/com/tlog/viewmodel/sns/SnsViewModel.kt
+++ Tlog/app/src/main/java/com/tlog/viewmodel/sns/SnsViewModel.kt
@@ -42,7 +42,8 @@ class SnsViewModel @Inject constructor(
             action = {
                 val result = repository.getFollowingPostList(lastPostId = lastPostId, size = size)
                 _postList.value = result.data.content
-                lastPostId = _postList.value[_postList.value.size - 1].postId
+                if (_postList.value.isNotEmpty())
+                    lastPostId = _postList.value[_postList.value.size - 1].postId
             }
         )
     }
