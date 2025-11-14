04624fd6895ebca7aaf361175af7aad1ef75e8c9
정찬 <125573608+cksckckcks@users.noreply.github.com>
2025-11-14 20:02:18 +0900

    feat: 지역 / 시군구 상수 값으로 변경 (#256)

---
+++ Tlog/app/src/main/java/com/tlog/data/local/RegionCode.kt
@@ -0,0 +1,545 @@
+package com.tlog.data.local
+
+object RegionCode {
+    // 광역시/도 코드
+    const val SEOUL = 1
+    const val GYEONGGI = 2
+    const val INCHEON = 3
+    const val BUSAN = 4
+    const val DAEGU = 5
+    const val GWANGJU = 6
+    const val DAEJEON = 7
+    const val ULSAN = 8
+    const val SEJONG = 9
+    const val GANGWON = 10
+    const val CHUNGBUK = 11
+    const val CHUNGNAM = 12
+    const val GYEONGBUK = 13
+    const val GYEONGNAM = 14
+    const val JEONBUK = 15
+    const val JEONNAM = 16
+    const val JEJU = 17
+
+    // 서울 (100번대)
+    const val SEOUL_GANGNAM = 101
+    const val SEOUL_GANGDONG = 102
+    const val SEOUL_GANGBUK = 103
+    const val SEOUL_GANGSEO = 104
+    const val SEOUL_GWANAK = 105
+    const val SEOUL_GWANGJIN = 106
+    const val SEOUL_GURO = 107
+    const val SEOUL_GEUMCHEON = 108
+    const val SEOUL_NOWON = 109
+    const val SEOUL_DOBONG = 110
+    const val SEOUL_DONGDAEMUN = 111
+    const val SEOUL_DONGJAK = 112
+    const val SEOUL_MAPO = 113
+    const val SEOUL_SEODAEMUN = 114
+    const val SEOUL_SEOCHO = 115
+    const val SEOUL_SEONGDONG = 116
+    const val SEOUL_SEONGBUK = 117
+    const val SEOUL_SONGPA = 118
+    const val SEOUL_YANGCHEON = 119
+    const val SEOUL_YEONGDEUNGPO = 120
+    const val SEOUL_YONGSAN = 121
+    const val SEOUL_EUNPYEONG = 122
+    const val SEOUL_JONGNO = 123
+    const val SEOUL_JUNG = 124
+    const val SEOUL_JUNGNANG = 125
+
+    // 경기 (200번대)
+    const val GYEONGGI_SUWON = 201
+    const val GYEONGGI_SEONGNAM = 202
+    const val GYEONGGI_GOYANG = 203
+    const val GYEONGGI_YONGIN = 204
+    const val GYEONGGI_BUCHEON = 205
+    const val GYEONGGI_ANSAN = 206
+    const val GYEONGGI_ANYANG = 207
+    const val GYEONGGI_NAMYANGJU = 208
+    const val GYEONGGI_HWASEONG = 209
+    const val GYEONGGI_PYEONGTAEK = 210
+    const val GYEONGGI_UIJEONGBU = 211
+    const val GYEONGGI_SIHEUNG = 212
+    const val GYEONGGI_PAJU = 213
+    const val GYEONGGI_GIMPO = 214
+    const val GYEONGGI_GWANGJU = 215
+    const val GYEONGGI_GWANGMYEONG = 216
+    const val GYEONGGI_GUNPO = 217
+    const val GYEONGGI_OSAN = 218
+    const val GYEONGGI_ICHEON = 219
+    const val GYEONGGI_ANSEONG = 220
+    const val GYEONGGI_UIWANG = 221
+    const val GYEONGGI_HANAM = 222
+    const val GYEONGGI_YEOJU = 223
+    const val GYEONGGI_YANGPYEONG = 224
+    const val GYEONGGI_DONGDUCHEON = 225
+    const val GYEONGGI_GWACHEON = 226
+    const val GYEONGGI_GAPYEONG = 227
+    const val GYEONGGI_YANGJU = 228
+    const val GYEONGGI_POCHEON = 229
+    const val GYEONGGI_YEONCHEON = 230
+
+    // 인천 (300번대)
+    const val INCHEON_GYEYANG = 301
+    const val INCHEON_NAMDONG = 302
+    const val INCHEON_DONG = 303
+    const val INCHEON_MICHUHOL = 304
+    const val INCHEON_BUPYEONG = 305
+    const val INCHEON_SEO = 306
+    const val INCHEON_YEONSU = 307
+    const val INCHEON_JUNG = 308
+
+    // 부산 (400번대)
+    const val BUSAN_GANGSEO = 401
+    const val BUSAN_GEUMJEONG = 402
+    const val BUSAN_GIJANG = 403
+    const val BUSAN_NAM = 404
+    const val BUSAN_DONG = 405
+    const val BUSAN_DONGNAE = 406
+    const val BUSAN_BUSANJIN = 407
+    const val BUSAN_BUK = 408
+    const val BUSAN_SASANG = 409
+    const val BUSAN_SAHA = 410
+    const val BUSAN_SEO = 411
+    const val BUSAN_SUYEONG = 412
+    const val BUSAN_YEONJE = 413
+    const val BUSAN_YEONGDO = 414
+    const val BUSAN_JUNG = 415
+    const val BUSAN_HAEUNDAE = 416
+
+    // 대구 (500번대)
+    const val DAEGU_NAM = 501
+    const val DAEGU_DALSEO = 502
+    const val DAEGU_DALSEONG = 503
+    const val DAEGU_DONG = 504
+    const val DAEGU_BUK = 505
+    const val DAEGU_SEO = 506
+    const val DAEGU_SUSEONG = 507
+    const val DAEGU_JUNG = 508
+
+    // 광주 (600번대)
+    const val GWANGJU_GWANGSAN = 601
+    const val GWANGJU_NAM = 602
+    const val GWANGJU_DONG = 603
+    const val GWANGJU_BUK = 604
+    const val GWANGJU_SEO = 605
+
+    // 대전 (700번대)
+    const val DAEJEON_DAEDEOK = 701
+    const val DAEJEON_DONG = 702
+    const val DAEJEON_SEO = 703
+    const val DAEJEON_YUSEONG = 704
+    const val DAEJEON_JUNG = 705
+
+    // 울산 (800번대)
+    const val ULSAN_NAM = 801
+    const val ULSAN_DONG = 802
+    const val ULSAN_BUK = 803
+    const val ULSAN_JUNG = 804
+    const val ULSAN_ULJU = 805
+
+    // 강원 (1000번대)
+    const val GANGWON_CHUNCHEON = 1001
+    const val GANGWON_WONJU = 1002
+    const val GANGWON_GANGNEUNG = 1003
+    const val GANGWON_DONGHAE = 1004
+    const val GANGWON_TAEBAEK = 1005
+    const val GANGWON_SOKCHO = 1006
+    const val GANGWON_SAMCHEOK = 1007
+    const val GANGWON_HONGCHEON = 1008
+    const val GANGWON_HOENGSEONG = 1009
+    const val GANGWON_YEONGWOL = 1010
+    const val GANGWON_PYEONGCHANG = 1011
+    const val GANGWON_JEONGSEON = 1012
+    const val GANGWON_CHEORWON = 1013
+    const val GANGWON_HWACHEON = 1014
+    const val GANGWON_YANGGU = 1015
+    const val GANGWON_INJE = 1016
+    const val GANGWON_GOSEONG = 1017
+    const val GANGWON_YANGYANG = 1018
+
+    // 충북 (1100번대)
+    const val CHUNGBUK_CHEONGJU = 1101
+    const val CHUNGBUK_CHUNGJU = 1102
+    const val CHUNGBUK_JECHEON = 1103
+    const val CHUNGBUK_BOEUN = 1104
+    const val CHUNGBUK_OKCHEON = 1105
+    const val CHUNGBUK_YEONGDONG = 1106
+    const val CHUNGBUK_JEUNGPYEONG = 1107
+    const val CHUNGBUK_JINCHEON = 1108
+    const val CHUNGBUK_GOESAN = 1109
+    const val CHUNGBUK_EUMSEONG = 1110
+    const val CHUNGBUK_DANYANG = 1111
+
+    // 충남 (1200번대)
+    const val CHUNGNAM_CHEONAN = 1201
+    const val CHUNGNAM_GONGJU = 1202
+    const val CHUNGNAM_BORYEONG = 1203
+    const val CHUNGNAM_ASAN = 1204
+    const val CHUNGNAM_SEOSAN = 1205
+    const val CHUNGNAM_NONSAN = 1206
+    const val CHUNGNAM_GYERYONG = 1207
+    const val CHUNGNAM_DANGJIN = 1208
+    const val CHUNGNAM_GEUMSAN = 1209
+    const val CHUNGNAM_BUYEO = 1210
+    const val CHUNGNAM_SEOCHEON = 1211
+    const val CHUNGNAM_CHEONGYANG = 1212
+    const val CHUNGNAM_HONGSEONG = 1213
+    const val CHUNGNAM_YESAN = 1214
+    const val CHUNGNAM_TAEAN = 1215
+
+    // 경북 (1300번대)
+    const val GYEONGBUK_POHANG = 1301
+    const val GYEONGBUK_GYEONGJU = 1302
+    const val GYEONGBUK_GIMCHEON = 1303
+    const val GYEONGBUK_ANDONG = 1304
+    const val GYEONGBUK_GUMI = 1305
+    const val GYEONGBUK_YEONGJU = 1306
+    const val GYEONGBUK_YEONGCHEON = 1307
+    const val GYEONGBUK_SANGJU = 1308
+    const val GYEONGBUK_MUNGYEONG = 1309
+    const val GYEONGBUK_GYEONGSAN = 1310
+    const val GYEONGBUK_UISEONG = 1311
+    const val GYEONGBUK_CHEONGSONG = 1312
+    const val GYEONGBUK_YEONGYANG = 1313
+    const val GYEONGBUK_YEONGDEOK = 1314
+    const val GYEONGBUK_CHEONGDO = 1315
+    const val GYEONGBUK_GORYEONG = 1316
+    const val GYEONGBUK_SEONGJU = 1317
+    const val GYEONGBUK_CHILGOK = 1318
+    const val GYEONGBUK_YECHEON = 1319
+    const val GYEONGBUK_BONGHWA = 1320
+    const val GYEONGBUK_ULJIN = 1321
+    const val GYEONGBUK_ULLEUNG = 1322
+    const val GYEONGBUK_GUNWI = 1323
+
+    // 경남 (1400번대)
+    const val GYEONGNAM_CHANGWON = 1401
+    const val GYEONGNAM_JINJU = 1402
+    const val GYEONGNAM_TONGYEONG = 1403
+    const val GYEONGNAM_SACHEON = 1404
+    const val GYEONGNAM_GIMHAE = 1405
+    const val GYEONGNAM_MILYANG = 1406
+    const val GYEONGNAM_GEOJE = 1407
+    const val GYEONGNAM_YANGSAN = 1408
+    const val GYEONGNAM_UIRYEONG = 1409
+    const val GYEONGNAM_HAMAN = 1410
+    const val GYEONGNAM_CHANGNYEONG = 1411
+    const val GYEONGNAM_GOSEONG = 1412
+    const val GYEONGNAM_NAMHAE = 1413
+    const val GYEONGNAM_HADONG = 1414
+    const val GYEONGNAM_SANCHEONG = 1415
+    const val GYEONGNAM_HAMYANG = 1416
+    const val GYEONGNAM_GEOCHANG = 1417
+    const val GYEONGNAM_HAPCHEON = 1418
+
+    // 전북 (1500번대)
+    const val JEONBUK_JEONJU = 1501
+    const val JEONBUK_GUNSAN = 1502
+    const val JEONBUK_IKSAN = 1503
+    const val JEONBUK_JEONGEUP = 1504
+    const val JEONBUK_NAMWON = 1505
+    const val JEONBUK_GIMJE = 1506
+    const val JEONBUK_WANJU = 1507
+    const val JEONBUK_JINAN = 1508
+    const val JEONBUK_MUJU = 1509
+    const val JEONBUK_JANGSU = 1510
+    const val JEONBUK_IMSIL = 1511
+    const val JEONBUK_SUNCHANG = 1512
+    const val JEONBUK_GOCHANG = 1513
+    const val JEONBUK_BUAN = 1514
+
+    // 전남 (1600번대)
+    const val JEONNAM_MOKPO = 1601
+    const val JEONNAM_YEOSU = 1602
+    const val JEONNAM_SUNCHEON = 1603
+    const val JEONNAM_NAJU = 1604
+    const val JEONNAM_GWANGYANG = 1605
+    const val JEONNAM_DAMYANG = 1606
+    const val JEONNAM_GOKSEONG = 1607
+    const val JEONNAM_GURYE = 1608
+    const val JEONNAM_GOHEUNG = 1609
+    const val JEONNAM_BOSEONG = 1610
+    const val JEONNAM_HWASUN = 1611
+    const val JEONNAM_JANGHEUNG = 1612
+    const val JEONNAM_GANGJIN = 1613
+    const val JEONNAM_HAENAM = 1614
+    const val JEONNAM_YEONGAM = 1615
+    const val JEONNAM_MUAN = 1616
+    const val JEONNAM_HAMPYEONG = 1617
+    const val JEONNAM_YEONGGWANG = 1618
+    const val JEONNAM_JANGSEONG = 1619
+    const val JEONNAM_WANDO = 1620
+    const val JEONNAM_JINDO = 1621
+    const val JEONNAM_SINAN = 1622
+
+    // 제주 (1700번대)
+    const val JEJU_JEJU = 1701
+    const val JEJU_SEOGWIPO = 1702
+
+    // 변환 맵
+    private val regionMap = mapOf(
+        "서울전체" to SEOUL,
+        "서울강남구" to SEOUL_GANGNAM,
+        "서울강동구" to SEOUL_GANGDONG,
+        "서울강북구" to SEOUL_GANGBUK,
+        "서울강서구" to SEOUL_GANGSEO,
+        "서울관악구" to SEOUL_GWANAK,
+        "서울광진구" to SEOUL_GWANGJIN,
+        "서울구로구" to SEOUL_GURO,
+        "서울금천구" to SEOUL_GEUMCHEON,
+        "서울노원구" to SEOUL_NOWON,
+        "서울도봉구" to SEOUL_DOBONG,
+        "서울동대문구" to SEOUL_DONGDAEMUN,
+        "서울동작구" to SEOUL_DONGJAK,
+        "서울마포구" to SEOUL_MAPO,
+        "서울서대문구" to SEOUL_SEODAEMUN,
+        "서울서초구" to SEOUL_SEOCHO,
+        "서울성동구" to SEOUL_SEONGDONG,
+        "서울성북구" to SEOUL_SEONGBUK,
+        "서울송파구" to SEOUL_SONGPA,
+        "서울양천구" to SEOUL_YANGCHEON,
+        "서울영등포구" to SEOUL_YEONGDEUNGPO,
+        "서울용산구" to SEOUL_YONGSAN,
+        "서울은평구" to SEOUL_EUNPYEONG,
+        "서울종로구" to SEOUL_JONGNO,
+        "서울중구" to SEOUL_JUNG,
+        "서울중랑구" to SEOUL_JUNGNANG,
+
+        "경기전체" to GYEONGGI,
+        "경기수원시" to GYEONGGI_SUWON,
+        "경기성남시" to GYEONGGI_SEONGNAM,
+        "경기고양시" to GYEONGGI_GOYANG,
+        "경기용인시" to GYEONGGI_YONGIN,
+        "경기부천시" to GYEONGGI_BUCHEON,
+        "경기안산시" to GYEONGGI_ANSAN,
+        "경기안양시" to GYEONGGI_ANYANG,
+        "경기남양주시" to GYEONGGI_NAMYANGJU,
+        "경기화성시" to GYEONGGI_HWASEONG,
+        "경기평택시" to GYEONGGI_PYEONGTAEK,
+        "경기의정부시" to GYEONGGI_UIJEONGBU,
+        "경기시흥시" to GYEONGGI_SIHEUNG,
+        "경기파주시" to GYEONGGI_PAJU,
+        "경기김포시" to GYEONGGI_GIMPO,
+        "경기광주시" to GYEONGGI_GWANGJU,
+        "경기광명시" to GYEONGGI_GWANGMYEONG,
+        "경기군포시" to GYEONGGI_GUNPO,
+        "경기오산시" to GYEONGGI_OSAN,
+        "경기이천시" to GYEONGGI_ICHEON,
+        "경기안성시" to GYEONGGI_ANSEONG,
+        "경기의왕시" to GYEONGGI_UIWANG,
+        "경기하남시" to GYEONGGI_HANAM,
+        "경기여주시" to GYEONGGI_YEOJU,
+        "경기양평군" to GYEONGGI_YANGPYEONG,
+        "경기동두천시" to GYEONGGI_DONGDUCHEON,
+        "경기과천시" to GYEONGGI_GWACHEON,
+        "경기가평군" to GYEONGGI_GAPYEONG,
+        "경기양주시" to GYEONGGI_YANGJU,
+        "경기포천시" to GYEONGGI_POCHEON,
+        "경기연천군" to GYEONGGI_YEONCHEON,
+
+        "인천전체" to INCHEON,
+        "인천계양구" to INCHEON_GYEYANG,
+        "인천남동구" to INCHEON_NAMDONG,
+        "인천동구" to INCHEON_DONG,
+        "인천미추홀구" to INCHEON_MICHUHOL,
+        "인천부평구" to INCHEON_BUPYEONG,
+        "인천서구" to INCHEON_SEO,
+        "인천연수구" to INCHEON_YEONSU,
+        "인천중구" to INCHEON_JUNG,
+
+        "부산전체" to BUSAN,
+        "부산강서구" to BUSAN_GANGSEO,
+        "부산금정구" to BUSAN_GEUMJEONG,
+        "부산기장군" to BUSAN_GIJANG,
+        "부산남구" to BUSAN_NAM,
+        "부산동구" to BUSAN_DONG,
+        "부산동래구" to BUSAN_DONGNAE,
+        "부산부산진구" to BUSAN_BUSANJIN,
+        "부산북구" to BUSAN_BUK,
+        "부산사상구" to BUSAN_SASANG,
+        "부산사하구" to BUSAN_SAHA,
+        "부산서구" to BUSAN_SEO,
+        "부산수영구" to BUSAN_SUYEONG,
+        "부산연제구" to BUSAN_YEONJE,
+        "부산영도구" to BUSAN_YEONGDO,
+        "부산중구" to BUSAN_JUNG,
+        "부산해운대구" to BUSAN_HAEUNDAE,
+
+        "대구전체" to DAEGU,
+        "대구남구" to DAEGU_NAM,
+        "대구달서구" to DAEGU_DALSEO,
+        "대구달성군" to DAEGU_DALSEONG,
+        "대구동구" to DAEGU_DONG,
+        "대구북구" to DAEGU_BUK,
+        "대구서구" to DAEGU_SEO,
+        "대구수성구" to DAEGU_SUSEONG,
+        "대구중구" to DAEGU_JUNG,
+
+        "광주전체" to GWANGJU,
+        "광주광산구" to GWANGJU_GWANGSAN,
+        "광주남구" to GWANGJU_NAM,
+        "광주동구" to GWANGJU_DONG,
+        "광주북구" to GWANGJU_BUK,
+        "광주서구" to GWANGJU_SEO,
+
+        "대전전체" to DAEJEON,
+        "대전대덕구" to DAEJEON_DAEDEOK,
+        "대전동구" to DAEJEON_DONG,
+        "대전서구" to DAEJEON_SEO,
+        "대전유성구" to DAEJEON_YUSEONG,
+        "대전중구" to DAEJEON_JUNG,
+
+        "울산전체" to ULSAN,
+        "울산남구" to ULSAN_NAM,
+        "울산동구" to ULSAN_DONG,
+        "울산북구" to ULSAN_BUK,
+        "울산중구" to ULSAN_JUNG,
+        "울산울주군" to ULSAN_ULJU,
+
+        "세종전체" to SEJONG,
+
+        "강원전체" to GANGWON,
+        "강원춘천시" to GANGWON_CHUNCHEON,
+        "강원원주시" to GANGWON_WONJU,
+        "강원강릉시" to GANGWON_GANGNEUNG,
+        "강원동해시" to GANGWON_DONGHAE,
+        "강원태백시" to GANGWON_TAEBAEK,
+        "강원속초시" to GANGWON_SOKCHO,
+        "강원삼척시" to GANGWON_SAMCHEOK,
+        "강원홍천군" to GANGWON_HONGCHEON,
+        "강원횡성군" to GANGWON_HOENGSEONG,
+        "강원영월군" to GANGWON_YEONGWOL,
+        "강원평창군" to GANGWON_PYEONGCHANG,
+        "강원정선군" to GANGWON_JEONGSEON,
+        "강원철원군" to GANGWON_CHEORWON,
+        "강원화천군" to GANGWON_HWACHEON,
+        "강원양구군" to GANGWON_YANGGU,
+        "강원인제군" to GANGWON_INJE,
+        "강원고성군" to GANGWON_GOSEONG,
+        "강원양양군" to GANGWON_YANGYANG,
+
+        "충북전체" to CHUNGBUK,
+        "충북청주시" to CHUNGBUK_CHEONGJU,
+        "충북충주시" to CHUNGBUK_CHUNGJU,
+        "충북제천시" to CHUNGBUK_JECHEON,
+        "충북보은군" to CHUNGBUK_BOEUN,
+        "충북옥천군" to CHUNGBUK_OKCHEON,
+        "충북영동군" to CHUNGBUK_YEONGDONG,
+        "충북증평군" to CHUNGBUK_JEUNGPYEONG,
+        "충북진천군" to CHUNGBUK_JINCHEON,
+        "충북괴산군" to CHUNGBUK_GOESAN,
+        "충북음성군" to CHUNGBUK_EUMSEONG,
+        "충북단양군" to CHUNGBUK_DANYANG,
+
+        "충남전체" to CHUNGNAM,
+        "충남천안시" to CHUNGNAM_CHEONAN,
+        "충남공주시" to CHUNGNAM_GONGJU,
+        "충남보령시" to CHUNGNAM_BORYEONG,
+        "충남아산시" to CHUNGNAM_ASAN,
+        "충남서산시" to CHUNGNAM_SEOSAN,
+        "충남논산시" to CHUNGNAM_NONSAN,
+        "충남계룡시" to CHUNGNAM_GYERYONG,
+        "충남당진시" to CHUNGNAM_DANGJIN,
+        "충남금산군" to CHUNGNAM_GEUMSAN,
+        "충남부여군" to CHUNGNAM_BUYEO,
+        "충남서천군" to CHUNGNAM_SEOCHEON,
+        "충남청양군" to CHUNGNAM_CHEONGYANG,
+        "충남홍성군" to CHUNGNAM_HONGSEONG,
+        "충남예산군" to CHUNGNAM_YESAN,
+        "충남태안군" to CHUNGNAM_TAEAN,
+
+        "경북전체" to GYEONGBUK,
+        "경북포항시" to GYEONGBUK_POHANG,
+        "경북경주시" to GYEONGBUK_GYEONGJU,
+        "경북김천시" to GYEONGBUK_GIMCHEON,
+        "경북안동시" to GYEONGBUK_ANDONG,
+        "경북구미시" to GYEONGBUK_GUMI,
+        "경북영주시" to GYEONGBUK_YEONGJU,
+        "경북영천시" to GYEONGBUK_YEONGCHEON,
+        "경북상주시" to GYEONGBUK_SANGJU,
+        "경북문경시" to GYEONGBUK_MUNGYEONG,
+        "경북경산시" to GYEONGBUK_GYEONGSAN,
+        "경북의성군" to GYEONGBUK_UISEONG,
+        "경북청송군" to GYEONGBUK_CHEONGSONG,
+        "경북영양군" to GYEONGBUK_YEONGYANG,
+        "경북영덕군" to GYEONGBUK_YEONGDEOK,
+        "경북청도군" to GYEONGBUK_CHEONGDO,
+        "경북고령군" to GYEONGBUK_GORYEONG,
+        "경북성주군" to GYEONGBUK_SEONGJU,
+        "경북칠곡군" to GYEONGBUK_CHILGOK,
+        "경북예천군" to GYEONGBUK_YECHEON,
+        "경북봉화군" to GYEONGBUK_BONGHWA,
+        "경북울진군" to GYEONGBUK_ULJIN,
+        "경북울릉군" to GYEONGBUK_ULLEUNG,
+        "경북군위군" to GYEONGBUK_GUNWI,
+
+        "경남전체" to GYEONGNAM,
+        "경남창원시" to GYEONGNAM_CHANGWON,
+        "경남진주시" to GYEONGNAM_JINJU,
+        "경남통영시" to GYEONGNAM_TONGYEONG,
+        "경남사천시" to GYEONGNAM_SACHEON,
+        "경남김해시" to GYEONGNAM_GIMHAE,
+        "경남밀양시" to GYEONGNAM_MILYANG,
+        "경남거제시" to GYEONGNAM_GEOJE,
+        "경남양산시" to GYEONGNAM_YANGSAN,
+        "경남의령군" to GYEONGNAM_UIRYEONG,
+        "경남함안군" to GYEONGNAM_HAMAN,
+        "경남창녕군" to GYEONGNAM_CHANGNYEONG,
+        "경남고성군" to GYEONGNAM_GOSEONG,
+        "경남남해군" to GYEONGNAM_NAMHAE,
+        "경남하동군" to GYEONGNAM_HADONG,
+        "경남산청군" to GYEONGNAM_SANCHEONG,
+        "경남함양군" to GYEONGNAM_HAMYANG,
+        "경남거창군" to GYEONGNAM_GEOCHANG,
+        "경남합천군" to GYEONGNAM_HAPCHEON,
+
+        "전북전체" to JEONBUK,
+        "전북전주시" to JEONBUK_JEONJU,
+        "전북군산시" to JEONBUK_GUNSAN,
+        "전북익산시" to JEONBUK_IKSAN,
+        "전북정읍시" to JEONBUK_JEONGEUP,
+        "전북남원시" to JEONBUK_NAMWON,
+        "전북김제시" to JEONBUK_GIMJE,
+        "전북완주군" to JEONBUK_WANJU,
+        "전북진안군" to JEONBUK_JINAN,
+        "전북무주군" to JEONBUK_MUJU,
+        "전북장수군" to JEONBUK_JANGSU,
+        "전북임실군" to JEONBUK_IMSIL,
+        "전북순창군" to JEONBUK_SUNCHANG,
+        "전북고창군" to JEONBUK_GOCHANG,
+        "전북부안군" to JEONBUK_BUAN,
+
+        "전남전체" to JEONNAM,
+        "전남목포시" to JEONNAM_MOKPO,
+        "전남여수시" to JEONNAM_YEOSU,
+        "전남순천시" to JEONNAM_SUNCHEON,
+        "전남나주시" to JEONNAM_NAJU,
+        "전남광양시" to JEONNAM_GWANGYANG,
+        "전남담양군" to JEONNAM_DAMYANG,
+        "전남곡성군" to JEONNAM_GOKSEONG,
+        "전남구례군" to JEONNAM_GURYE,
+        "전남고흥군" to JEONNAM_GOHEUNG,
+        "전남보성군" to JEONNAM_BOSEONG,
+        "전남화순군" to JEONNAM_HWASUN,
+        "전남장흥군" to JEONNAM_JANGHEUNG,
+        "전남강진군" to JEONNAM_GANGJIN,
+        "전남해남군" to JEONNAM_HAENAM,
+        "전남영암군" to JEONNAM_YEONGAM,
+        "전남무안군" to JEONNAM_MUAN,
+        "전남함평군" to JEONNAM_HAMPYEONG,
+        "전남영광군" to JEONNAM_YEONGGWANG,
+        "전남장성군" to JEONNAM_JANGSEONG,
+        "전남완도군" to JEONNAM_WANDO,
+        "전남진도군" to JEONNAM_JINDO,
+        "전남신안군" to JEONNAM_SINAN,
+
+        "제주전체" to JEJU,
+        "제주제주시" to JEJU_JEJU,
+        "제주서귀포시" to JEJU_SEOGWIPO
+    )
+
+    fun fromStringOrNull(region: String): Int {
+        return regionMap[region] ?: 0 // 추후 가드 ?
+    }
+}
